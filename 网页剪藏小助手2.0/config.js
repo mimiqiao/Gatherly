b0137eb331770029a244329a956aff0251482fd7
waytoAGI <mimiqiao@gmail.com>
2025-01-04 17:42:53 +0800

    Delete 网页剪藏小助手2.0 directory

--- 网页剪藏小助手2.0/config.js
+++
@@ -1,6 +0,0 @@
-export const FEISHU_CONFIG = {
-  APP_ID: 'cli_a7fc0e59d938100c',
-  APP_SECRET: 'OIWOlxV9TKzDYrobnPmgdfACg8lRa42k',
-  BASE_ID: 'VVGIbSMr7aPoqEsYg0Kc7noCn3c',
-  TABLE_ID: 'tblr92PoZbPIw6OJ'
-}; 
